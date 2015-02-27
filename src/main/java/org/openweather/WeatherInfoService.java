926ac6268c75a0d4c5440e6baa25975995d792d7
bdshadow <bdshadow@gmail.com>
2015-02-28 02:23:11 +0300

    Initial commit

---
+++ src/main/java/org/openweather/WeatherInfoService.java
@@ -0,0 +1,12 @@
+package org.openweather;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class WeatherInfoService {
+	
+	public void test() {
+		System.out.println("Hello world!!!");
+	}
+
+}
